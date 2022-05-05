8efa84e50a9d1d92f200106fb259c0ae018400bb
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-05-05 02:26:01 +0200

    Finishing Slider and Announcement

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Announcement from '../Components/Announcement'
 import NavBar from '../Components/NavBar'
+import Slider from '../Components/Slider'
 
 
 const Home = () => {
     return (
         <div>
+            <Announcement />
             <NavBar />
+            <Slider />
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,24 @@
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-photo/fashion-model-demonstrate-trendy-wear-accessories-casual-red-jacket-black-short-dress_273443-361.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-photo/excited-white-girl-bright-stylish-glasses-posing-pink-dreamy-curly-woman-playing-with-her-ginger-hair-laughing_197531-11045.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/excited-white-girl-bright-stylish-glasses-posing-pink-dreamy-curly-woman-playing-with-her-ginger-hair-laughing_197531-11045.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
