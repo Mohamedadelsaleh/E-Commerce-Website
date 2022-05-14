a002ea9c984de5741777996edde1850bf1cc62bf
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-05-14 16:31:36 +0200

    Adding new page with new functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React,{Fragment} from "react";
 import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
 function App() {
   return (
     <Fragment>
-        <Home />
+        <ProductList />
     </Fragment>
   );
 }
